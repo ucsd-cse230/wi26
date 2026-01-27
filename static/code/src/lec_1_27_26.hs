abdaa410183a09075e9508a79813bd0d4c1b5e3d
Ranjit Jhala <rjhala@ucsd.edu>
2026-01-27 09:31:45 -0800

    update page

---
+++ static/code/src/lec_1_27_26.hs
@@ -0,0 +1,9 @@
+{- HLINT ignore "Eta reduce" -}
+{- HLINT ignore "Use uncurry" -}
+{- HLINT ignore "Use :" -}
+{- HLINT ignore "Use id" -}
+
+module Lec_1_27_26  where
+
+inc :: Int -> Int
+inc n = n + 1
